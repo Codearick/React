fe058eee54da321b8433f64fbf3e847551393e1c
Codearick <rohan.gautam650@gmail.com>
2024-03-04 02:12:54 +0530

    add multiselection feature

--- 04-Accordion/src/components/Accordion.jsx
+++ 04-Accordion/src/components/Accordion.jsx
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import { useState } from 'react';
 import data from './data';
 
 //Single selection
@@ -6,37 +6,63 @@ import data from './data';
 
 const Accordion = () => {
     const [selected, setSelected] = useState();
-    
-    const handleSingleSelection = (currentId) =>{
-        console.log("Current id: ",currentId)
+    const [multiSelect, setMultiSelect] = useState(false);
+    const [multiple, setMultiple] = useState([]);
+    const [img, setImg] = useState("+")
+
+
+    const handleSingleSelection = (currentId) => {
+        console.log("Current id: ", currentId)
         setSelected(currentId === selected ? null : currentId);
     }
-    
-  return (
-    <div className='wrapper'>
-        {
-            data && data.length > 0 ?
-            data.map(dataItem => <div key={dataItem.id} className='item'>
-                <div onClick={()=>handleSingleSelection(dataItem.id)} className="title flex justify-center gap-10 px-10 py-7">
-                    <div className='w-[40vw] '>
-                    <h3 className='font-extrabold text-xl'>{dataItem.question}</h3>
-                    </div>
-                    <div>
-                    <span className='font-extrabold text-2xl'>+</span>
-                    </div>
-                </div>
-                {
-                    selected === dataItem.id ? 
-                    <div className='answer flex align-center justify-center transition duration-1000 ease-in-out delay-200'>
-                    <span className='w-[44vw] px-10 py-7 border bg-slate-600 text-xl'>{dataItem.answer}</span>
-                    </div>
-                    : null
-                }
-            </div>)
-             : <div> No Data Found !</div>
-        }
-    </div>
-  )
+
+    const handleMultiSelection = async (currentId) => {
+        let copyMultiple = [...multiple];
+        const indexOfCurrentId = copyMultiple.indexOf(currentId);
+
+        if (indexOfCurrentId === -1) copyMultiple.push(currentId);
+        else  copyMultiple.splice(indexOfCurrentId,1);
+
+        setMultiple(copyMultiple);
+        console.log(multiple);
+        return "On";
+    }
+
+    return (
+        <div className='wrapper flex flex-col justify-center align center'>
+            <div className='mx-auto m-4 cursor-pointer'>
+                <button onClick={() => setMultiSelect(!multiSelect)} className='border p-4'>
+                    Multiselection
+                </button>
+            </div>
+            {
+                data && data.length > 0 ?
+                    data.map(dataItem => <div key={dataItem.id} className='item'>
+                        <div onClick={multiSelect
+                            ? () => handleMultiSelection(dataItem.id)
+                            : () => handleSingleSelection(dataItem.id)
+                        } className="title flex justify-center ">
+                            <div className='border flex w-[80vw] py-5 px-4 my-2'>
+                                <div className='w-[95%]'>
+                                    <h3 className='font-extrabold text-xl'>{dataItem.question}</h3>
+                                </div>
+                                <div className=''>
+                                    <span className='font-extrabold text-2xl'>{img}</span>
+                                </div>
+                            </div>
+                        </div>
+                        {
+                        selected === dataItem.id || multiple.indexOf(dataItem.id) !== -1 ? 
+                         <div className={`answer mx-auto my-0 flex align-center justify-center`}>
+                             <span className='w-[80vw] px-10 py-7 border text-xl'>{dataItem.answer}</span>
+                        </div>
+                        : null
+                        }
+                    </div>)
+                    : <div> No Data Found !</div>
+            }
+        </div>
+    )
 }
 
 export default Accordion;
